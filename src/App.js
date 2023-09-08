15dac48360385e52175b3a0cf75d8805382a22b5
Mokaddis-ALIF <mokaddis.alif@gmail.com>
2023-09-08 11:45:12 +0600

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [init, setInit] = useState(true);
+  console.log('1--');
+
+  useEffect(() => { }, [])
+  console.log('2--');
+  if (init) {
+    alert('ok')
+    console.log('in--');
+    setInit(false)
+  }
+  //   useEffect(() => {
+  //     if (init) {
+  //       alert('ok')
+  //       console.log('in--');
+  //       setInit(false)
+  //     }
+  // }, [init])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ok</h1>
+      <button type='submit' onClick={e => setInit(true)} >click</button>
+    </>
   );
 }
 
